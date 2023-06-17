2ec28a5be2ab0470f30073624b689286510bfcc5
Roughnick <JGStutson@gmail.com>
2023-06-17 02:50:00 -0600

    Updated blogpage with dynamic blog data

--- src/App.js
+++ src/App.js
@@ -2,39 +2,42 @@ import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import Blogpost from "./pages/BlogPost";
+import { PostProvider } from "./providers/post";
 
 const Layout = () => {
-  return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
-  );
+	return (
+		<>
+			<Navbar />
+			<Outlet />
+		</>
+	);
 };
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/post/:id",
-        element: <Blogpost />,
-      },
-    ],
-  },
+	{
+		path: "/",
+		element: <Layout />,
+		children: [
+			{
+				path: "/",
+				element: <Home />,
+			},
+			{
+				path: "/post/:id",
+				element: <Blogpost />,
+			},
+		],
+	},
 ]);
 
 function App() {
-  return (
-    <div>
-      <RouterProvider router={router} />
-    </div>
-  );
+	return (
+		<PostProvider>
+			<div>
+				<RouterProvider router={router} />
+			</div>
+		</PostProvider>
+	);
 }
 
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,48 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { usePost } from "../providers/post";
 
 const Home = () => {
-  const posts = [
-    {
-      id: 1,
-      title: "Title Blah",
-      desc: "Descriptions Blah",
-    },
-    {
-      id: 2,
-      title: "Title Blah",
-      desc: "Descriptions Blah",
-    },
-    {
-      id: 3,
-      title: "Title Blah",
-      desc: "Descriptions Blah",
-    },
-    {
-      id: 4,
-      title: "Title Blah",
-      desc: "Descriptions Blah",
-    },
-  ];
-  return (
-    <div className="home">
-      <div className="posts">
-        {posts.map((post) => (
-          <div className="post" key={post.id}>
-            <div className="content">
-              <Link to={`/post/${post.id}`}>
-                <h1>{post.title}</h1>
-              </Link>
-              <p>{post.desc}</p>
-              <Link to={`/post/${post.id}`}>
-                <button>Read More</button>
-              </Link>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+	const posts = usePost();
+
+	return (
+		<div className="home">
+			<div className="posts">
+				{posts.map((post) => (
+					<div className="post" key={post.id}>
+						<div className="content">
+							<Link to={`/post/${post.id}`}>
+								<h1>{post.title}</h1>
+							</Link>
+							<p>{post.desc}</p>
+							<Link to={`/post/${post.id}`}>
+								<button>Read More</button>
+							</Link>
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default Home;
