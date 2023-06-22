643f13076a2759b83c67f0c6d8366c3d1e4f334c
Jonathan Stutson <jonathan.stutson@extendhealth.com>
2023-06-22 13:15:41 -0600

    Removed console logs and added blog data

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -1,20 +1,17 @@
-import { Console } from "node:console";
 import { readFile, writeFile } from "node:fs/promises";
 
 export default async function (request, response) {
-	const file = JSON.parse(await readFile("./pages/posts.json"));
-	switch (request.method) {
-		case "GET":
-			console.log(file);
-			return response.status(200).json(file);
-		case "POST":
-			console.log(request.body);
-			file.push({
-				id: file.at(-1).id + 1 || 1,
-				title: request.body.title,
-				body: request.body.body,
-			});
-			await writeFile("./pages/posts.json", JSON.stringify(file));
-			return response.status(201).json(file);
-	}
+  const file = JSON.parse(await readFile("./pages/posts.json"));
+  switch (request.method) {
+    case "GET":
+      return response.status(200).json(file);
+    case "POST":
+      file.push({
+        id: file.at(-1).id + 1 || 1,
+        title: request.body.title,
+        body: request.body.body,
+      });
+      await writeFile("./pages/posts.json", JSON.stringify(file));
+      return response.status(201).json(file);
+  }
 }
